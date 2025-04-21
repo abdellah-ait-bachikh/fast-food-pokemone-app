dd02585facdac8773bf8ef401715d975da22c26b
abdellah ait bachikh <abdo.jhon2023@gmail.com>
2025-04-21 18:16:28 +0100

    active-link-style

--- src/components/main/Aside.tsx
+++ src/components/main/Aside.tsx
@@ -3,8 +3,10 @@ import { setAsideOpen } from "@/redux/slace/appSlice";
 import { TinitialState } from "@/type/statesTypes";
 import classNames from "classnames";
 import React, { useEffect } from "react";
+import { CiLogout } from "react-icons/ci";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { IoMdSettings } from "react-icons/io";
 
 const Aside = () => {
   const dispatch = useDispatch();
@@ -26,7 +28,7 @@ const Aside = () => {
     >
       <div className="w-full h-full flex flex-col items-center p-4">
         <div>logo</div>
-        <div className="w-full flex flex-col gap-2 mt-16">
+        <div className="w-full flex-grow flex flex-col gap-2 mt-16">
           {links.map((e) => (
             <div key={e.name} className="w-full ">
               <NavLink
@@ -52,6 +54,53 @@ const Aside = () => {
               </NavLink>
             </div>
           ))}
+        </div>
+        <div className="w-full flex flex-col gap-3">
+          <div className="w-full ">
+            <NavLink
+              to="/settings"
+              className={classNames(
+                "flex items-center overflow-hidden border-3 dark:border-slate-600   gap-3 bg-amber-500/60 dark:bg-slate-950 rounded-xl transition-width  h-[47px] p-[11px]",
+                {
+                  "w-[47px] justify-start": !isAsideOpen,
+                  "w-full ": isAsideOpen,
+                }
+              )}
+            >
+              <span className={classNames("text-xl ")}>
+                {React.createElement(IoMdSettings)}
+              </span>
+              <span
+                className={classNames(
+                  "font-semibold text-[17px] tracking-widest "
+                )}
+              >
+                Settings
+              </span>
+            </NavLink>
+          </div>
+          <div className="w-full ">
+            <button
+              className={classNames(
+                "flex items-center overflow-hidden border-3 dark:border-slate-600   gap-3 bg-danger-300/60 dark:bg-danger-950 rounded-xl transition-width  h-[47px] p-[11px]",
+                {
+                  "w-[47px] justify-start": !isAsideOpen,
+                  "w-full ": isAsideOpen,
+                }
+              )}
+            >
+              <span className={classNames("text-xl ")}>
+                {React.createElement(CiLogout)}
+              </span>
+              <span
+                className={classNames(
+                  "font-semibold text-[17px] tracking-widest "
+                )}
+              >
+                Logout
+              </span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
