b6da673e58d86336af12864b65b483ff075c77ef
abdellah ait bachikh <abdo.jhon2023@gmail.com>
2025-05-05 20:49:03 +0100

    pagination > day

--- src/components/main/day/Table.tsx
+++ src/components/main/day/Table.tsx
@@ -1,4 +1,4 @@
-import { DatePicker, } from '@heroui/react'
+import { DatePicker, Pagination, } from '@heroui/react'
 
 const Table = () => {
     return (
@@ -6,10 +6,10 @@ const Table = () => {
             <div className='w-full flex justify-center '>
                 <div className='w-fit text-nowrap px-2 bg-white dark:bg-slate-900 rounded-xl p-2.5'><DatePicker fullWidth={false} /></div>
             </div>
-            <div className='rounded-xl w-full bg-white dark:bg-slate-900 p-2.5 mt-4 overflow-x-auto'>
-                <table className='w-full'>
+            <div className='rounded-xl w-full bg-white dark:bg-slate-900 p-2.5 mt-4  h-[60vh] flex flex-col gap-3'>
+                <div className='w-full flex-1 overflow-auto'>  <table className='w-full'>
                     <thead>
-                        <tr >
+                        <tr  >
                             <th className='font-semibold text-center py-1.5 w-fit text-nowrap px-3'>N</th>
                             <th className='font-semibold text-center py-1.5 w-fit text-nowrap px-3'>Depart</th>
                             <th className='font-semibold text-center py-1.5 w-fit text-nowrap px-3'>Fin</th>
@@ -20,7 +20,7 @@ const Table = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className=''>
+                        <tr className='hover:bg-gray-200 dark:hover:bg-slate-800'>
                             <td className='text-center py-1.5 w-fit text-nowrap px-3'>1</td>
                             <td className='text-center py-1.5 w-fit text-nowrap px-3'>11/12/2025 a 17:30</td>
                             <td className='text-center py-1.5 w-fit text-nowrap px-3'>ENCOR</td>
@@ -33,7 +33,7 @@ const Table = () => {
                                 </div>
                             </td>
                         </tr>
-                        <tr className=''>
+                        <tr className='hover:bg-gray-200 dark:hover:bg-slate-800'>
                             <td className='text-center py-1.5 w-fit text-nowrap px-3'>1</td>
                             <td className='text-center py-1.5 w-fit text-nowrap px-3'>11/12/2025 a 17:30</td>
                             <td className='text-center py-1.5 w-fit text-nowrap px-3'>11/12/2025 a 00:36</td>
@@ -48,7 +48,9 @@ const Table = () => {
                         </tr>
                     </tbody>
                     <tfoot></tfoot>
-                </table>
+                </table></div>
+                <div className=''><Pagination showControls initialPage={1} total={10} /></div>
+
             </div>
         </div>
     )
